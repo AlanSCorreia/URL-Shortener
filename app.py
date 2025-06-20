4e1606bac28be5d77dd3135f915c9214953f61c2
AlanSCorreia <alancorreia110@gmail.com>
2025-06-20 10:54:05 -0300

    feat: valida se a url_curta existe no banco e, caso não exista, redireciona para uma página informando que não existe

--- app.py
+++ app.py
@@ -2,45 +2,49 @@ from os import path, makedirs, getcwd
 import re
 import sqlite3
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, url_for
 
 
 diretorio_atual = getcwd()
 diretorio_db = path.join(diretorio_atual, "db")
 
-if not path.exists(diretorio_db): makedirs(diretorio_db)
+if not path.exists(diretorio_db):
+    makedirs(diretorio_db)
 
 
 conexao = sqlite3.connect("db\\database.db", check_same_thread=False)
 cursor = conexao.cursor()
 
 with conexao:
-    conexao.cursor().execute("""create table if not exists url (id integer,
-                                        url_curta varchar(50) not null,
-                                        url_redirecionada text not null,
-                                        visualizacoes integer,
-                                        PRIMARY KEY(id AUTOINCREMENT));""")
+    conexao.cursor().execute("""create table if not exists url
+                                (id integer,
+                                 url_curta varchar(50) not null,
+                                 url_redirecionada text not null,
+                                 visualizacoes integer,
+                                 PRIMARY KEY(id AUTOINCREMENT));""")
     
 
-def cadastrar_url(url_curta, url_redirecionada, visualizacoes=0):
+def cadastrar_url(url_curta, url_redirecionada, visualizacoes=0) -> None:
 
     cursor.execute("""INSERT INTO url (url_curta, url_redirecionada, visualizacoes)
                       VALUES (?, ?, ?);""", (url_curta, url_redirecionada, visualizacoes))
     conexao.commit()
 
 
-def obter_url(url_curta):
+def obter_url_redirecionada(url_curta) -> str:
     cursor.execute("""SELECT url_redirecionada
                       FROM url
                       WHERE url_curta = ?;""", (url_curta,))
     retorno = cursor.fetchone()
+        
+    return retorno
 
+
+def atualizar_contagem_visualizacao(url_curta: str) -> None:
     cursor.execute("""UPDATE url
                       SET visualizacoes = visualizacoes + 1
                       WHERE url_curta = ?;""", (url_curta,))
     conexao.commit()
-        
-    return retorno
 
 
 def formatar_rota(url: str) -> str:
@@ -53,6 +57,11 @@ def formatar_rota(url: str) -> str:
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return "Placeholder"
+
+
 @app.route("/cadastrar")
 def cadastrar_rota():
     dados_recebidos = request.get_json()
@@ -61,16 +70,31 @@ def cadastrar_rota():
     if re.match(regex_url_valida, dados_recebidos["url_redirecionada"]):
         url_curta = dados_recebidos["url_curta"]
         url_redirecionada = formatar_rota(dados_recebidos["url_redirecionada"])
+
         cadastrar_url(url_curta, url_redirecionada)
         return "Criado", "201"
     
     return "Falha no processo de cadastro", "400"
 
 
+@app.route("/nao-existe")
+def rota_nao_existe():
+    return "<h1>404 Not Found</h1>\nA rota que você tentou não existe"
+
+
 @app.route("/<url_curta>")
 def redirecionar_rota(url_curta: str):
-    url = obter_url(url_curta)[0]
-    return redirect(url)
+
+    if url_curta is None:
+        return redirect(url_for("index"))
+    
+    url_redirecionada = obter_url_redirecionada(url_curta)
+    
+    if url_redirecionada:
+        atualizar_contagem_visualizacao(url_curta)
+        return redirect(url_redirecionada[0])
+
+    return redirect(url_for("rota_nao_existe"))
 
 
 if __name__ == "__main__":
